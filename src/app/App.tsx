b62cc561ef9225e4f42ea4deba4fb20f8ec7be7c
bekazmr <bekazmr@gmail.com>
2024-11-03 04:34:11 +0600

    add i18n translation , sidebar + navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,12 +3,20 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
 import { Navbar } from "widgets/Navbar";
 import { AppRouter } from "./providers/router";
+import { Sidebar } from "widgets/Sidebar";
+import { Suspense } from "react";
+
 const App = () => {
   const { theme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
-      <Navbar />
-      <AppRouter />
+      <Suspense fallback="">
+        <Navbar />
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   );
 };
