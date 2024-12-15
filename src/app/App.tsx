f1b0a02b9b9c001e277cf08f374fc637372c1b8c
bekazmr <bekazmr@gmail.com>
2024-12-16 03:54:12 +0600

    error Boundary

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,10 +4,11 @@ import { useTheme } from "app/providers/ThemeProvider";
 import { Navbar } from "widgets/Navbar";
 import { AppRouter } from "./providers/router";
 import { Sidebar } from "widgets/Sidebar";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 
 const App = () => {
   const { theme } = useTheme();
+
   return (
     <div className={classNames("app", {}, [theme])}>
       <Suspense fallback="">
