4ab624ceef7377fd0c1929ab1b9c144dd039b08f
bekazmr <bekazmr@gmail.com>
2025-01-12 05:00:50 +0600

    add loki screenshot test

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,6 +1,6 @@
 import webpack, {RuleSetRule} from "webpack";
-import { BuildPath } from "../build/types/config";
 import path from "path";
+import { BuildPath } from "../build/types/config";
 import { buildCssLoader } from "../build/loaders/buildCssLoaders";
 export default ({ config }: { config: webpack.Configuration }) => {
   const paths: BuildPath = {
@@ -11,7 +11,6 @@ export default ({ config }: { config: webpack.Configuration }) => {
   };
   config.resolve.modules.push(paths.src);
   config.resolve.extensions.push('.ts', '.tsx');
-  config.module.rules.push(buildCssLoader(true));
 
   config.module.rules =  config.module.rules.map((rule:RuleSetRule) =>{
     if(/svg/.test(rule.test as string)) {
@@ -20,10 +19,11 @@ export default ({ config }: { config: webpack.Configuration }) => {
     return  rule
   });
 
-  config.module.rules.push({
+  config.module?.rules?.push({
     test: /\.svg$/,
     use: ["@svgr/webpack"],
   });
+  config.module.rules.push(buildCssLoader(true));
 
     return config;
 };
